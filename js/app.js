ca44e7e693ab4de91c10ed6ca77e4818018ff4da
marizoo <marizoo@meow.woof>
2022-01-29 12:34:51 +0700

    section: Sessions: 100% + added collapse navbar JS code

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     smartSpeed: 1000,
     navText: ["<", ">"],
     responsive: {
-      0: { nav: false },
+      0: { nav: false, dots: true },
       768: { nav: true },
     },
   });
@@ -18,14 +18,26 @@ $(document).ready(function () {
   $("#project-slider").owlCarousel({
     loop: true,
     margin: 0,
-    nav: false,
-    dots: false,
+    nav: true,
+    dots: true,
     smartSpeed: 1000,
     margin: 24,
+    navText: ["<", ">"],
     responsive: {
-      0: {},
-      768: {},
+      0: { items: 1, nav: false, margin: 8 },
+      768: { items: 2, margin: 16 },
       1140: { items: 2, center: true },
     },
   });
+});
+
+// to collapse navbar
+// $("#navbarSupportedContent").on("show.bs.collapse", function () {
+//   $("a.nav-link").click(function () {
+//     $("#navbarSupportedContent").collapse("hide");
+//   });
+// });
+
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
 });
